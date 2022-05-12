f0e4567a22061da297f75079aac79786e5a8f003
sajeeshmt <102586742+sajeeshmt@users.noreply.github.com>
2022-05-12 20:43:47 +0530

    todolist
    todolist

---
+++ script.js
@@ -0,0 +1,20 @@
+
+function loginValidate(checkSuccess) {
+    let userName = document.getElementById("userid").value;
+    let password = document.getElementById("password").value;
+    checkSuccess(userName,password);
+} 
+function onSuccess(uname,pwd) {
+        if (uname == "admin" && pwd == "12345") {
+            
+            document.querySelector("form").action = "./todolist.html";
+        }
+        else {
+            alert("Invalid login");
+        }
+}
+
+function onLogin() {
+    loginValidate(onSuccess);
+} 
+ 
